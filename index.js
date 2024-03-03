6dc518fddf492dc3a17534d8ded9bd55c7dc577e
Purna Chandra <royr8936@gmail.com>
2024-03-03 21:08:00 +0530

    First version

---
+++ index.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+console.clear()
+console.log(`
+👋 Hello!
+I am Purna Chandra......
+
+----------------------------------------------------
+"Passionate full-stack JavaScript developer with 
+expertise in both frontend and backend technologies. 
+Proficient in Node.js, React, and MongoDB, with a 
+knack for crafting scalable and efficient 
+web applications. Committed to continuous learning 
+and staying updated with the latest trends in technology."
+-----------------------------------------------------
+
+Github : https://github.com/purna-dev/
+npm : https://www.npmjs.com/~purnachandra
+
+`)
